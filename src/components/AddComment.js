bc1afceb0c2284f97e2e3106a2f002f28042ecc2
Yasmin Dal Medico <yasmin.dm@gmail.com>
2023-02-15 13:06:45 +0100

    comments

---
+++ src/components/AddComment.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AddComment = ()=>{
+
+    const notYet = ()=>{
+        alert ("Under construction")
+    }
+
+    return <button onClick={notYet}>Add Comment</button>
+}
+
+export default AddComment;
--- src/components/Book.js
+++ src/components/Book.js
@@ -1,6 +1,8 @@
 import React from "react";
 
+import AddComment from "./AddComment";
 import "./Book.module.css";
+import ShowComments from "./ShowComments";
 
 const Book = (props)=>{
 const {book} = props;
@@ -9,6 +11,10 @@ return <article >
     <h1 className="center">{book.name}</h1>
     <img src={book.image} alt="cover of the book" /> 
     <p>{book.description}</p>
+    <section className="center">
+        <ShowComments />
+        <AddComment />
+    </section>
 </article>
 };
 
