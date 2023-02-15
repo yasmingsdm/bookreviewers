54ff023457f2998df65aec8a07020e9bdcf76dc4
Yasmin Dal Medico <yasmin.dm@gmail.com>
2023-02-15 11:43:20 +0100

    add book button

---
+++ src/components/ButtonBook.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ButtonBook = ()=>{
+
+    const addBook = ()=>{
+        alert ("Under construction")
+    }
+     return <button onClick={addBook}>
+        Add Book
+    </button>
+}
+
+export default ButtonBook;
