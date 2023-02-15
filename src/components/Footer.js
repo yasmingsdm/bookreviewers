72c56e080149aaec667dd3174721661f9d1d2a9f
Yasmin Dal Medico <yasmin.dm@gmail.com>
2023-02-15 09:41:02 +0100

    footer

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import './Footer.module.css';
+
+const Footer = () =>{
+    return <footer className="center">
+        <p className="bold">Created by <a href="https://github.com/yasmingsdm" target="_blank" rel="noreferrer">Yasmin Dal Medico</a></p>
+    </footer>
+};
+
+export default Footer;
